a35c0aeb52b44ec7fdef5b0b34cccbb865ee0272
Fabio Zuber <fabio.zuber92@gmail.com>
2016-11-10 14:14:16 +0100

    feat(groupManager): sample powershell execution

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import jetpack from 'fs-jetpack'; // module loaded from npm
 import $ from "jquery";
 import selectize from 'selectize'; // module loaded from npm
 import env from './env';
+import child_process from "child_process";
 
 console.log('Loaded environment variables:', env);
 var app = remote.app;
@@ -41,6 +42,7 @@ function buttonHandler(event){
             //TODO ps script to get user details from AD
             break;
         case('start-groupmembership-script'):
+            startGroupMembership();
             //TODO start powershell process with group manager tool
             break;
         case('open-email'):
@@ -49,6 +51,21 @@ function buttonHandler(event){
         default:
             alert('Button handler not implemented for this button')
     }
+}
+
+function startGroupMembership() {
+    var spawn =  child_process.spawn,child;
+    child = spawn("powershell.exe",["powershell.exe -ExecutionPolicy Bypass c:\\temp\\test.ps1"]);
+    child.stdout.on("data",function(data){
+        console.log("Powershell Data: " + data);
+    });
+    child.stderr.on("data",function(data){
+        console.log("Powershell Errors: " + data);
+    });
+    child.on("exit",function(){
+        console.log("Powershell Script finished");
+    });
+    child.stdin.end(); //end input
 }
 
 
