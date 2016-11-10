a9bdf30f90bb561d2df0af382ba5dd161456112d
Fabio Zuber <fabio.zuber92@gmail.com>
2016-11-10 12:12:54 +0100

    feat: add button handler

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-// Here is the starting point for your application code.
-// All stuff below is just to show you how it works. You can delete all of it.
-
 // Use new ES6 modules syntax for everything.
+
 import os from 'os'; // native node.js module
 import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
@@ -13,14 +11,14 @@ console.log('Loaded environment variables:', env);
 var app = remote.app;
 var appDir = jetpack.cwd(app.getAppPath());
 
-// Holy crap! This is browser window with HTML and stuff, but I can read
-// here files like it is node.js! Welcome to Electron world :)
+
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('env-name').innerHTML = env.name;
 
     $('#select-users').selectize({
+        plugins: ['remove_button','restore_on_backspace'],
         delimiter: ';',
         persist: false,
         create: function(input) {
@@ -30,4 +28,27 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+    var buttons = document.getElementsByTagName('button');
+    Array.from(buttons).forEach(button => {
+        button.addEventListener('click', buttonHandler);
+    });
 });
+
+function buttonHandler(event){
+    switch(event.target.id){
+        case('get-user-details'):
+            //TODO ps script to get user details from AD
+            break;
+        case('start-groupmembership-script'):
+            //TODO start powershell process with group manager tool
+            break;
+        case('open-email'):
+            //TODO email with recipients form user selection
+            break;
+        default:
+            alert('Button handler not implemented for this button')
+    }
+}
+
+
