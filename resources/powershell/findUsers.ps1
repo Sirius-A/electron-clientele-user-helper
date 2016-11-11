9eb706b2dcd12a66f7833026533ad643fd761f6e
Fabio Zuber <fabio.zuber@siemens.com>
2016-11-11 14:33:22 +0100

    feat(findUsers) construct filter from user list

--- resources/powershell/findUsers.ps1
+++ resources/powershell/findUsers.ps1
@@ -1,8 +1,52 @@
 Param(
-  Parameter(Mandatory=$True]
-   [string[]]$users
+   [Parameter(Mandatory=$True)]
+   [string[]] $users
 )
-$emailRegex = "\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"
-$filter =
-[scriptblock]::create(($users| foreach {"(mail -eq '$_')"}) -join ' -or ')
-Get-ADUser -f $filter
+
+
+Function Construct-Filter
+{
+  Param
+  (
+    # List of, e-mail adrsses and GID's
+    [Parameter(Mandatory=$true)]
+    [String[]]$users
+  )
+  [string]$filter = "(&(objectClass=User)"
+
+  $emailRegex = "\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"
+  $GIDRegex = "^[\w\d]*$"
+  $winCredentialsRegex = "^[\w\d]*\\[\w\d]*$"
+
+  foreach ($user in $users)
+  {
+    if ($user -match $emailRegex){
+      $filter = "$filter(mail=$user)"
+    }elseif($user -match $GIDRegex){
+      $filter = "$filter(siemens-gid=$user)"
+    }elseif($user -match $winCredentialsRegex){
+      $filter = "$filter(samaccountname=$user)"
+    }else{
+      Write-host "Error finding pattern for $user" -ForegroundColor Red
+    }
+  }
+
+  return "$filter)" #Close final bracket and return string
+}
+
+$serachfilter = Construct-Filter $users
+
+$searcher = [adsisearcher]""
+$searcher.filter = $filter
+$searcher.PropertiesToLoad.addRange(("siemens-gid",
+"displayname",
+"employeetype",
+"department",
+"givenname",
+"sn",
+"displaynameprintable",
+"samaccountname",
+"mail",
+"objectcategory",
+"telephonenumber"
+))
